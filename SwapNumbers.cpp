b1cd3adc82e00b021414d2a4dea70087d62eeaa6
Sh-Ifti <shahariarifti16@gmail.com>
2025-05-11 00:35:37 +0600

    Swapping using pointers

--- SwapNumbers.cpp
+++ SwapNumbers.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 using namespace std;
 
+void swap(int* x, int* y)
+{
+    if (x != y)
+    {
+        *x = *x ^ *y;
+        *y = *x ^ *y;
+        *x = *x ^ *y;
+    }
+}
+
 int main()
 {
     int a = 10, b = 20;
 
     cout << "Before swap " << " a: " << a << " and b: " << b << endl;
 
-    a = a ^ b; 
-    b = a ^ b; 
-    a = a ^ b; 
+    swap(&a, &b);
 
     cout << "After swap " << " a: " << a << " and b: " << b << endl;
 
