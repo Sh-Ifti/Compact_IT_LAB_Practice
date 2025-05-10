0585bdb0fc3d460a7e709ba094eb31dea114c706
Sh-Ifti <shahariarifti16@gmail.com>
2025-05-11 00:25:01 +0600

    Swapping using xor

--- SwapNumbers.cpp
+++ SwapNumbers.cpp
@@ -7,9 +7,9 @@ int main()
 
     cout << "Before swap " << " a: " << a << " and b: " << b << endl;
 
-    a = a + b; // a + b = 30
-    b = a - b; // a = 30, b = 20; b = a - b = 30 - 20 = 10, which is prev value of a
-    a = a - b; // a = 30, b = 10; a = 30 - 10 = 20; which is the prev value of b
+    a = a ^ b; 
+    b = a ^ b; 
+    a = a ^ b; 
 
     cout << "After swap " << " a: " << a << " and b: " << b << endl;
 
